0cfe583e3a7b4030e0b27e2aa4edcb9fcf0e8a6d
saurabhgit2 <saurabhgithub0@gmail.com>
2025-11-28 15:33:56 +1300

    Upload code of Fibonacci series using function

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-print("Let us write a simple code for Fibonacci series without using any function")
 
-a=0 #initializing the first term of the series
-b=1 #initializing second term
-n = int(input("Enter the number of terms in Fibonacci series: "))  #Prompting user to enter the number of terms for Fibonacci series
+n=int(input("Enter the number of terms: "))  #Prompting Accepting user input
+def fibonacci(n):  #function declaration to print Fibonacci series
+    a,b=0,1   #first and second term in sequence declared
+    print(a)
+    print(b)
+    for i in range(2,n):  #for loop using to print series from index 2 till the n
+        c=a+b
+        a=b
+        b=c
+        print(c) #Printing series from index 2
 
-print(a) #Printing the first term at index 0
-print(b) #Printing index 1 term
-for i in range(2,n):    #for loop to print sequence from index 2 till n
-    c=a+b  #variable used to store the sum of previous two terms
-    a=b  #swapping the values to calculate next value of c(fibonacci series)
-    b=c  #same reason as above
-    print(c) #printing sequence from index 2 till n
-    
-    
+fibonacci(n) #Function called
